0ce34b89485c5fbb0dfa2059478be72e47ac9b12
hsxian <940100027@qq.com>
2026-06-17 18:16:48 +0800

    优化

--- main/media/music/provider/cenguigui_resource.cc
+++ main/media/music/provider/cenguigui_resource.cc
@@ -11,60 +11,19 @@
 
 #define TAG "CenguiguiResource"
 #ifdef CONFIG_CENGUIGUI_RESOURCE_ADDRESS
-_Static_assert(sizeof(CONFIG_CENGUIGUI_RESOURCE_ADDRESS) > 1, "CONFIG_CENGUIGUI_RESOURCE_ADDRESS不能为空");
+_Static_assert(sizeof(CONFIG_CENGUIGUI_RESOURCE_ADDRESS) > 1,
+               "CONFIG_CENGUIGUI_RESOURCE_ADDRESS不能为空");
 #else
 #error "CONFIG_CENGUIGUI_RESOURCE_ADDRESS is not defined"
 #endif
-// 辅助函数：替换字符串中的子串
 
 bool CenguiguiResource::Search(const QueryBase& query, std::vector<Music*>& music_list) {
-    RestfulClient restful_client;
-    std::string url = std::format("{}?name={}&page={}&limit={}", CONFIG_CENGUIGUI_RESOURCE_ADDRESS,
-                                  query.keyword, query.page, query.page_size);
-    ESP_LOGI(TAG, "url: %s", url.c_str());
     std::string keyword = restful_client.UrlEncode(query.keyword);
-    url = std::format("{}?name={}&page={}&limit={}", CONFIG_CENGUIGUI_RESOURCE_ADDRESS, keyword, query.page,
-                      query.page_size);
-    bool success = false;
-    std::string response = restful_client.Get(url);
-    if (response.empty()) {
-        return success;
-    }
-    StringHelper string_helper;
-    response = string_helper.ReplaceStringAll(response, "http://", "https://");
-
-    auto json = cJSON_Parse(response.c_str());
-    if (!json) {
-        ESP_LOGE(TAG, "Failed to parse JSON");
-        return success;
-    }
-
-    cJSON* code = cJSON_GetObjectItem(json, "code");
-    if (code && cJSON_IsNumber(code) && code->valueint == 200) {
-        cJSON* data = cJSON_GetObjectItem(json, "data");
-
-        MusicHelper music_helper;
-        music_helper.FromJsonArray(data, music_list);
-        success = music_list.size() > 0;
-    } else {
-        ESP_LOGE(TAG, "Failed to parse JSON, code: %d", code->valueint);
-    }
-    cJSON_Delete(json);
-    return success;
-}
-
-std::string CenguiguiResource::Search(const std::string& params) {
-    RestfulClient restful_client;
-    std::string url = std::format("{}?{}", CONFIG_CENGUIGUI_RESOURCE_ADDRESS, params);
-    ESP_LOGI(TAG, "url: %s", url.c_str());
-
-    std::string response = restful_client.Get(url);
-    if (!response.empty()) {
-        StringHelper string_helper;
-        response = string_helper.ReplaceStringAll(response, "http://", "https://");
-    }
-
-    return response;
+    auto url = std::format("{}?name={}&page={}&limit={}", CONFIG_CENGUIGUI_RESOURCE_ADDRESS,
+                           keyword, query.page, query.page_size);
+    ESP_LOGI(TAG, "url: %s, keyword: %s", url.c_str(), query.keyword.c_str());
+   
+    return MusicResource::Search(url, music_list);
 }
 
 void CenguiguiResource::ParseLyricsFromJson(const std::string& json, Lyrics& lyrics) {
@@ -86,11 +45,7 @@ void CenguiguiResource::ParseLyricsFromJson(const std::string& json, Lyrics& lyr
 }
 void CenguiguiResource::ParseMusicFromJson(cJSON* item, Music& music) { music.FromJson(item); }
 
-std::string CenguiguiResource::GetUrl(Music& music) {
-    return music.url;
-}
-std::string CenguiguiResource::GetLyricsUrl(Music& music) {
-    return music.lrc;
-}
+std::string CenguiguiResource::GetUrl(Music& music) { return music.url; }
+std::string CenguiguiResource::GetLyricsUrl(Music& music) { return music.lrc; }
 
 #endif
--- main/media/music/provider/cenguigui_resource.h
+++ main/media/music/provider/cenguigui_resource.h
@@ -10,7 +10,6 @@ class CenguiguiResource : public MusicResource {
 public:
     virtual ~CenguiguiResource() = default;
     virtual bool Search(const QueryBase& query, std::vector<Music*>& music_list) override;
-    virtual std::string Search(const std::string & params) override;
     virtual std::string GetUrl(Music& music) override;
     virtual std::string GetLyricsUrl(Music& music) override;
     virtual void ParseMusicFromJson(cJSON* item, Music& music) override;
